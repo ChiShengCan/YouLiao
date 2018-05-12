cae6adc2d00094717ee323d3303310d30b813cdd
youyue <git@github.com:ChiShengCan/ZhiHuAds.git>
2018-05-12 11:16:24 +0800

    Revised on May 12,for add book-sort

--- app/src/main/java/com/youyue/csc/youliao/ui/fragment/book/BookFragment.kt
+++ app/src/main/java/com/youyue/csc/youliao/ui/fragment/book/BookFragment.kt
@@ -3,11 +3,13 @@ package com.youyue.csc.youliao.ui.fragment.book
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.youyue.csc.youliao.R
-import com.youyue.csc.youliao.adapter.BookTabLayoutAdapter
+import com.youyue.csc.youliao.adapter.BookTitleAdapter
 
 
 /**
@@ -17,13 +19,11 @@ import com.youyue.csc.youliao.adapter.BookTabLayoutAdapter
 class BookFragment : Fragment(){
 
 
-
-    private lateinit var fragments: MutableList<Fragment>
-    private lateinit var titles: MutableList<String>
-    private lateinit var boyBookFragment: BoyBookFragment
-    private lateinit var girlBookFragment: GirlBookFragment
-    private lateinit var famousBookFragment: FamousBookFragment
-    private  var tabLayoutAdapter: BookTabLayoutAdapter?=null
+    //左边的类别列表控件
+    var rvSort:RecyclerView?=null
+    var mLinearLayoutManager:LinearLayoutManager?=null
+    var mSortAdapter:BookTitleAdapter?=null
+    var mBookTitle:MutableList<String>?=null
 
 
 
@@ -31,6 +31,32 @@ class BookFragment : Fragment(){
 
 
         var view=inflater?.inflate(R.layout.fragment_book, container, false)
+
+
+        rvSort=view?.findViewById(R.id.rv_sort) as RecyclerView
+        mLinearLayoutManager= LinearLayoutManager(context)
+        rvSort?.layoutManager=mLinearLayoutManager
+
+        //添加分类标题的数据
+        mBookTitle= ArrayList<String>() as MutableList<String>?
+        mBookTitle?.add("男生")
+        mBookTitle?.add("女生")
+        mBookTitle?.add("出版")
+        mBookTitle?.add("漫画")
+        mSortAdapter= BookTitleAdapter(context,mBookTitle)
+        rvSort?.adapter=mSortAdapter
+        //设置条目点击监听
+        mSortAdapter?.setOnClickListener(object : BookTitleAdapter.ItemOnCkickListener{
+            override fun onClickListener(position: Int) {
+                //改变条目选中的状态
+                mSortAdapter?.setCheckedPosition(position)
+            }
+
+        })
+
+
+
+
 
 
         return view
